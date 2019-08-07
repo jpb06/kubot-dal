3f989d72d2eee1c190dfdf5759dc7b546d8925c3
jpb <jpb.06@outlook.com>
2019-08-07 06:05:43 +0200

    Extracting project management logic

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,15 @@
 ﻿/// <binding BeforeBuild='clean' AfterBuild='generatePackage, moveReadme' Clean='clean' />
 const gulp = require('gulp');
-const rimraf = require('rimraf');
-const fs = require('fs');
+const fsUtil = require('./project-apparatus/fs.util.js');
 
-gulp.task('generatePackage', () => {
-    const package = JSON.parse(fs.readFileSync('./package.json').toString());
-
-    let distPackage = {
-        name: package.name,
-        version: package.version,
-        description: package.version,
-        main: package.main,
-        types: package.types,
-        author: package.author,
-        dependencies: package.dependencies
-    };
-
-    fs.writeFile('./dist/package.json', JSON.stringify(distPackage, null, 2), 'utf8', (err) => {
-        if (err) console.log('Error while writing dist package.json file:', err);
-    });
+gulp.task('generatePackage', async () => {
+    await fsUtil.generatePackage();
 });
 
-gulp.task('moveReadme', () => {
-    fs.unlink('./dist/README.md', function (err) {
-        if (err === null || (err && err.code === 'ENOENT')) {
-            fs.createReadStream('./README.md').pipe(fs.createWriteStream('./dist/README.md'));
-        } else if (err) {
-            console.error('Error while moving readme:', err);
-        }
-    });
+gulp.task('moveReadme', async () => {
+    await fsUtil.moveReadme();
 });
 
-gulp.task('clean', () => {
-    rimraf('./dist', (err) => {
-        console.log('Error during clean:', err);
-    });
+gulp.task('clean', async () => {
+    await fsUtil.cleanDist();
 });
